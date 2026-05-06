feddd961af2ee90dd99cd08f7545d878e10f55bf
Sunil Kumar <sunil11qq11@gmail.com>
2026-05-06 11:39:25 +0530

    Add JavaScript Todo App

---
+++ todoapp.js
@@ -0,0 +1,44 @@
+let todoList = [
+  {
+    item: "Go to the college",
+    dueDate: "5/12/2024",
+  },
+  {
+    item: "Buy home",
+    dueDate: "5/12/2024",
+  },
+];
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector("#todo-input");
+  let dateElement = document.querySelector("#todo-date");
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  console.log(todoItem);
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = "";
+  dateElement.value = "";
+
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector(".todo-container");
+  let newHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    // let item = todoList[i].item;
+    // let dueDate = todoList[i].dueDate;
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+       
+       <span>${item}</span>
+       <span>${dueDate}</span>
+       <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+       displayItems();">Delete</button>
+        
+      `;
+  }
+  containerElement.innerHTML = newHtml;
+}
